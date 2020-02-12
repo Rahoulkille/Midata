7192d63e92c5be7117487437f5bab7e1e3019d52
Maurice <mauricelecordier@gmail.com>
2020-02-12 16:40:14 +0200

    added the ability toi create a user to log in with

--- src/knex.ts
+++ src/knex.ts
@@ -2,7 +2,14 @@ import knex from 'knex';
 import { Application } from './declarations';
 
 export default function (app: Application) {
-  const { client, connection } = app.get('postgres');
+  const { client } = app.get('postgres');
+
+  const connection = {
+    host : '127.0.0.1',
+    user : 'mauricelecordier',
+    password : '',
+    database : 'boilerplate'
+  }
   const db = knex({ client, connection });
 
   app.set('knexClient', db);
--- src/services/users/users.class.ts
+++ src/services/users/users.class.ts
@@ -1,11 +1,30 @@
 import { Service, KnexServiceOptions } from 'feathers-knex';
 import { Application } from '../../declarations';
+import { Params } from '@feathersjs/feathers';
 
-export class Users extends Service {
+interface UserData {
+  email: string;
+  password: string;
+}
+export class Users extends Service<UserData> {
   constructor(options: Partial<KnexServiceOptions>, app: Application) {
     super({
       ...options,
       name: 'users'
     });
+  }
+
+  create (data: UserData, params?: Params) {
+    // This is the information we want from the user signup data
+    const { email, password } = data;
+
+    // The complete user
+    const userData = {
+      email,
+      password,
+    };
+
+    // Call the original `create` method with existing `params` and new data
+    return super.create(userData, params);
   }
 }
